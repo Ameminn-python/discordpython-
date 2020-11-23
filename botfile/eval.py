478c0ed4c510af2a377c96f5fdb82f195340d03f
あめみん <63637891+Ameminn-python@users.noreply.github.com>
2020-11-24 08:06:45 +0900

    Update eval.py

--- botfile/eval.py
+++ botfile/eval.py
@@ -1,1 +1,89 @@
+from discord.ext import commands
+import asyncio
+import traceback
+import discord
+import inspect
+import textwrap
+import importlib
+from contextlib import redirect_stdout
+import io
+import os
+import re
+import sys
+import copy
+
+import discord
+from discord.ext import commands
+import io
+import traceback
+import textwrap
+from contextlib import redirect_stdout
+
+class cmd(commands.Cog):
+
+    def __init__(self,bot):
+        self.bot = bot
+        self._last_result = None
+
+    def cleanup_code(self, content):
+        """Automatically removes code blocks from the code."""
+        #  ```py\n``` を取り除く
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+
+        # remove `foo`
+        return content.strip('` \n')
+
+    @commands.command(pass_context=True,name='eval')
+    async def _eval(self, ctx, *, body: str):
+        """discord上で、Pythonコードを動かします。"""
+
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
+
+
+
+
+def setup(bot):
+    bot.add_cog(cmd(bot))
 
