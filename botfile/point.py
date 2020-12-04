f6c29578419be696827ac2c5812b2cd30e017d42
あめみん <63637891+Ameminn-python@users.noreply.github.com>
2020-12-04 14:05:44 +0900

    Update point.py

--- botfile/point.py
+++ botfile/point.py
@@ -1,1 +1,12 @@
+import disocrd
+form discord.ext import commands
+import gspread
 
+
+class Spread(commands.Cog):
+  
+  def __init__(self,bot):
+    slef.bot = bot
+    
+  def spread(self,key):
+    
